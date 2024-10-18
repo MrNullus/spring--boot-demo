3b0d1e590ca1fd7047c3ba47b3d123ae984d6237
Mr Punk da Silva <gustavojs417@gmail.com>
2024-10-18 19:58:47 -0300

    finish project

--- src/main/java/org/punkdomus/demo/dao/PersonDataAcessService.java
+++ src/main/java/org/punkdomus/demo/dao/PersonDataAcessService.java
@@ -1,6 +1,8 @@
 package org.punkdomus.demo.dao;
 
 import org.punkdomus.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +12,13 @@ import java.util.UUID;
 @Repository("postgres")
 public class PersonDataAcessService implements PersonDao {
 
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDataAcessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertPerson(UUID id, Person person) {
         return 0;
@@ -17,12 +26,22 @@ public class PersonDataAcessService implements PersonDao {
 
     @Override
     public List<Person> selectAllPeople() {
-        return List.of(new Person(UUID.randomUUID(), "FROM POSTGRES DB"));
+        final String sql = "SELECT ID, NAME FROM person";
+        return jdbcTemplate.query(sql, (result, error) -> {
+            Person person = new Person(UUID.fromString(result.getString("id")), result.getString("name"));
+            return person;
+        });
     }
 
     @Override
     public Optional<Person> selectPersonById(UUID id) {
-        return Optional.empty();
+        final String sql = "SELECT id, name FROM person WHERE id = ?";
+
+        Person person = jdbcTemplate.queryForObject(sql, new Object[]{id},(result, error) -> {
+            UUID personId = UUID.fromString(result.getString("id"));
+            return new Person(personId, result.getString("name"));
+        });
+        return Optional.ofNullable(person);
     }
 
     @Override
--- src/main/java/org/punkdomus/demo/datasource/PostgresDatasource.java
+++ src/main/java/org/punkdomus/demo/datasource/PostgresDatasource.java
@@ -1,5 +1,20 @@
 package org.punkdomus.demo.datasource;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class PostgresDatasource {
-    
+
+    @Bean
+    @ConfigurationProperties("app.datasource")
+    public HikariDataSource hikariDatasource() {
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .build();
+    }
 }
